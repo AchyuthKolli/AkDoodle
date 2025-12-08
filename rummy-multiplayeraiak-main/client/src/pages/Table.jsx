b99b6a5c5e5dada3730e5d70a452d1331d4d05eb
achyuthkolli7-develop <achyuthkolli7@gmail.com>
2025-12-09 04:13:56 +0530

    Update frontend and backend code

--- rummy-multiplayeraiak-main/client/src/pages/Table.jsx
+++ rummy-multiplayeraiak-main/client/src/pages/Table.jsx
@@ -33,17 +33,17 @@ import {
 import { toast } from "sonner";
 
 // ✅ ALL RUMMY COMPONENTS NOW UNDER games/rummy/
-import HandStrip from "../games/rummy/components/HandStrip.jsx";
-import TableDiagram from "../games/rummy/components/TableDiagram.jsx";
-import CasinoTable3D from "../games/rummy/components/CasinoTable3D.jsx";
-import PlayerProfile from "../games/rummy/components/PlayerProfile.jsx";
+import { HandStrip } from "../games/rummy/components/HandStrip.jsx";
+import { TableDiagram } from "../games/rummy/components/TableDiagram.jsx";
+import { CasinoTable3D } from "../games/rummy/components/CasinoTable3D.jsx";
+import { PlayerProfile } from "../games/rummy/components/PlayerProfile.jsx";
 import PlayingCard from "../games/rummy/components/PlayingCard.jsx";
-import GameRules from "../games/rummy/components/GameRules.jsx";
-import GameHistory from "../games/rummy/components/GameHistory.jsx";
+import { GameRules } from "../games/rummy/components/GameRules.jsx";
+import { GameHistory } from "../games/rummy/components/GameHistory.jsx";
 import SpectateControls from "../games/rummy/components/SpectateControls.jsx";
-import WildJokerRevealModal from "../games/rummy/components/WildJokerRevealModal.jsx";
-import ScoreboardModal from "../games/rummy/components/ScoreboardModal.jsx";
-import PointsTable from "../games/rummy/components/PointsTable.jsx";
+import { WildJokerRevealModal } from "../games/rummy/components/WildJokerRevealModal.jsx";
+import { ScoreboardModal } from "../games/rummy/components/ScoreboardModal.jsx";
+import { PointsTable } from "../games/rummy/components/PointsTable.jsx";
 import MeldBoard from "../games/rummy/components/MeldBoard.jsx";
 import VoicePanel from "../games/rummy/components/VoicePanel.jsx";
 import HistoryTable from "../games/rummy/components/HistoryTable.jsx";
