87488a0d1a5b3ce419b73f579a93ee66bd9698bc
achyuthkolli7-develop <achyuthkolli7@gmail.com>
2025-12-09 10:42:38 +0530

    Update frontend and backend code

--- rummy-multiplayeraiak-main/client/src/main.jsx
+++ rummy-multiplayeraiak-main/client/src/main.jsx
@@ -2,15 +2,8 @@ import "./index.css"; // if you have global CSS
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext";
 import App from "./App.jsx";
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
